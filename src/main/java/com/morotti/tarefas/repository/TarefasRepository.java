ded63d7f12e1bfe0fb25b506303f05cae5903c65
Daniel Morotti Vieira <morottivieirad@gmail.com>
2026-03-24 10:05:01 -0300

    Repository

---
+++ src/main/java/com/morotti/tarefas/repository/TarefasRepository.java
@@ -0,0 +1,9 @@
+package com.morotti.tarefas.repository;
+
+import com.morotti.tarefas.entity.TarefasEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefasRepository extends JpaRepository<TarefasEntity, Long> {
+}
